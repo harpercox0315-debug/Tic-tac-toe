fc908064863cae02c9cc1ffb0b02e790946c8bcc
harpercox0315-debug <harper.cox0315@gmail.com>
2026-06-15 17:55:57 -0400

    Add click event and alert function in Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,25 @@
+<body>
+  <div onclick="alert('Player X');">
+   Click here to find out if your X or O
+  </div>
+  </body>
+  <script>
+  </script>
+  </body>
+  </html>
+  </div>
+<script>
+  </script>
+  </body>
+  </html>
+  </div>
+<script>
+  function place (box) {
+    alert("you clicked here");
+  }
+  </script>
+<div>
+         
+         
 
+  
