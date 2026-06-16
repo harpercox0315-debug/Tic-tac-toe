464b219b5dfb83115f3a89a87da8d7bcbe36c724
harpercox0315-debug <harper.cox0315@gmail.com>
2026-06-16 14:23:39 -0400

    Refactor place function for player switching logic

--- Script.js
+++ Script.js
@@ -19,7 +19,81 @@
   }
   </script>
 <div>
-         
+<div class= "row">
+  <div onclick= "place(this)"></div>
+  <div onclick= "place(this)"></div>
+  <div onclick= "place(this)"></div>
+  </div>
+<div class="row">
+  <div onclick= "place(this)"></div>
+  <div onclick= "place(this)"></div>
+  <div onclick= "place(this)"></div>
+  <div onclick= "place(this)></div>
+<div>
+<div class="row">
+  <div onclick="place(this)></div>
+  <div onclick="place(this)></div>
+  <div onclick="place(this)></div>
+  </div>
+  </div>
+  </div>
+<script>
+  var currentPlayer= "O"
+function place(box)
+alert("You clicked here!");
+}
+</script>
+<script>
+  var currentPlayer= "O"
+function place(box) {
+  box.innerText= currentplayer;
+}
+</script>
+<script>
+  var currentPlayer= "O";
+function place(box) {
+  box.innerText= currentPlayer;
+  if(currentPlayer== "O") {
+    currentPlayer= "X";
+  }
+}
+</script>
+<script> 
+  var currentPlayer ="O";
+function place(box) {
+  box.innerText = currentPlayer;
+  if(currentPlayer == "O") {
+    currentPlayer = "X";
+  }
+  else {
+    currentPlayer = "O";
+  }
+}
+</script>
+<body>
+  <div class="row">
+  <div onclick="place(this)" ></div>
+  </div>
+<script>
+  var currentPlayer = "O";
+function place(box) {
+  if(box.innerText != "") return;
+  box.innerText = currentPlayer;
+  if(currentPlayer == "O") {
+    currentPlayer = "X";
+  }
+  else {
+  }
+        currentPlayer = "O";
+}
+}
+</script>
+  </body>
+
+  
+
+  
+  
          
 
   
